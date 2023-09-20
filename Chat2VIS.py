3b94eb8df1a88ff8c2602becb8779eab9893236f
frog-land <98231593+frog-land@users.noreply.github.com>
2023-09-21 09:16:45 +1200

    Add Code Llama

--- Chat2VIS.py
+++ Chat2VIS.py
@@ -13,13 +13,15 @@ import warnings
 warnings.filterwarnings("ignore")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_icon="chat2vis.png",layout="wide",page_title="Chat2VIS")
+
+
 st.markdown("<h1 style='text-align: center; font-weight:bold; font-family:comic sans ms; padding-top: 0rem;'> \
             Chat2VIS</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='text-align: center;padding-top: 0rem;'>Creating Visualisations using Natural Language \
-            with ChatGPT</h2>", unsafe_allow_html=True)
-st.sidebar.write(":clap: :red[*Code Llama model coming soon....*]")
+            with ChatGPT and Code Llama</h2>", unsafe_allow_html=True)
 
-st.sidebar.markdown('<a style="text-align: center;padding-top: 0rem;" href="mailto: i.build.apps.4.u@gmail.com">:email:</a>Paula Maddigan and Teo Susnjak', unsafe_allow_html=True)
+#st.sidebar.write(":clap: :red[*Code Llama model coming soon....*]")
+st.sidebar.markdown('<a style="text-align: center;padding-top: 0rem;" href="mailto: i.build.apps.4.u@gmail.com">:email:</a> Paula Maddigan and Teo Susnjak', unsafe_allow_html=True)
 st.sidebar.markdown("<h4  style='text-align: center;font-size:small;color:grey;padding-top: 0rem;padding-bottom: .2rem;'>Chat2VIS: Generating Data \
                     Visualisations via Natural Language using ChatGPT, Codex and GPT-3 \
                  Large Language Models </h4>", unsafe_allow_html=True)
@@ -31,7 +33,11 @@ Natural Language Text and Pre-Trained Large Language Models </h4>", unsafe_allow
 
 st.sidebar.caption("(https://doi.org/10.48550/arXiv.2303.14292)")
 
-available_models = {"ChatGPT-4": "gpt-4","ChatGPT-3.5": "gpt-3.5-turbo","GPT-3": "text-davinci-003",}
+st.sidebar.markdown('<a style="text-align: center;padding-top: 0rem;" href="https://blog.streamlit.io/chat2vis-ai-driven-visualisations-with-streamlit-and-natural-language">Blog </a> by Paula Maddigan', unsafe_allow_html=True)
+
+
+available_models = {"ChatGPT-4": "gpt-4","ChatGPT-3.5": "gpt-3.5-turbo","GPT-3": "text-davinci-003",
+                        "GPT-3.5 Instruct": "gpt-3.5-turbo-instruct","Code Llama":"CodeLlama-34b-Instruct-hf"}
 
 # List to hold datasets
 if "datasets" not in st.session_state:
@@ -49,22 +55,27 @@ else:
     # use the list already loaded
     datasets = st.session_state["datasets"]
 
-my_key = st.text_input(label = ":key: OpenAI Key:", help="Please ensure you have an OpenAI API account with credit. ChatGPT Plus subscription does not include API access.",type="password")
+key_col1,key_col2 = st.columns(2)
+openai_key = key_col1.text_input(label = ":key: OpenAI Key:", help="Required for ChatGPT-4, ChatGPT-3.5, GPT-3, GPT-3.5 Instruct.",type="password")
+hf_key = key_col2.text_input(label = ":hugging_face: HuggingFace Key:",help="Required for Code Llama", type="password")
 
 with st.sidebar:
     # First we want to choose the dataset, but we will fill it with choices once we've loaded one
     dataset_container = st.empty()
 
     # Add facility to upload a dataset
-    uploaded_file = st.file_uploader(":computer: Load a CSV file:", type="csv")
-    index_no=0
-    if uploaded_file is not None:
-        # Read in the data, add it to the list of available datasets
-        file_name = uploaded_file.name[:-4].capitalize()
-        datasets[file_name] = pd.read_csv(uploaded_file)
-        # Default for the radio buttons
-        index_no = len(datasets)-1
-
+    try:
+        uploaded_file = st.file_uploader(":computer: Load a CSV file:", type="csv")
+        index_no=0
+        if uploaded_file:
+            # Read in the data, add it to the list of available datasets. Give it a nice name.
+            file_name = uploaded_file.name[:-4].capitalize()
+            datasets[file_name] = pd.read_csv(uploaded_file)
+            # We want to default the radio button to the newly added dataset
+            index_no = len(datasets)-1
+    except Exception as e:
+        st.error("File failed to load. Please select a valid CSV file.")
+        print("File failed to load.\n" + str(e))
     # Radio buttons for dataset choice
     chosen_dataset = dataset_container.radio(":bar_chart: Choose your data:",datasets.keys(),index=index_no)#,horizontal=True,)
 
@@ -76,54 +87,65 @@ with st.sidebar:
         label = f"{model_desc} ({model_name})"
         key = f"key_{model_desc}"
         use_model[model_desc] = st.checkbox(label,value=True,key=key)
-
-# Text area for query
+ 
+ # Text area for query
 question = st.text_area(":eyes: What would you like to visualise?",height=10)
 go_btn = st.button("Go...")
 
 # Make a list of the models which have been selected
-#model_dict = {model_name: use_model for model_name, use_model in use_model.items() if use_model}
-#model_count = len(model_dict)
 model_list = [model_name for model_name, choose_model in use_model.items() if choose_model]
 model_count = len(model_list)
 
 # Execute chatbot query
 if go_btn and model_count > 0:
-    # Place for plots depending on how many models
-    plots = st.columns(model_count)
-    # Get the primer for this dataset
-    primer1,primer2 = get_primer(datasets[chosen_dataset],'datasets["'+ chosen_dataset + '"]')
-    # Format the question
-    question_to_ask = format_question(primer1,primer2 , question)    
-    # Create model, run the request and print the results
-    for plot_num, model_type in enumerate(model_list):
-        with plots[plot_num]:
-            st.subheader(model_type)
-            try:
-                # Run the question
-                answer=""
-                answer = run_request(question_to_ask, available_models[model_type], key=my_key)
-                # the answer is the completed Python script so add to the beginning of the script to it.
-                answer = primer2 + answer
-                plot_area = st.empty()
-                plot_area.pyplot(exec(answer))           
-            except Exception as e:
-                if type(e) == openai.error.APIError:
-                    st.error("OpenAI API Error. Please try again a short time later.")
-                elif type(e) == openai.error.Timeout:
-                    st.error("OpenAI API Error. Your request timed out. Please try again a short time later.")
-                elif type(e) == openai.error.RateLimitError:
-                    st.error("OpenAI API Error. You have exceeded your assigned rate limit.")
-                elif type(e) == openai.error.APIConnectionError:
-                    st.error("OpenAI API Error. Error connecting to services. Please check your network/proxy/firewall settings.")
-                elif type(e) == openai.error.InvalidRequestError:
-                    st.error("OpenAI API Error. Your request was malformed or missing required parameters.")
-                elif type(e) == openai.error.AuthenticationError:
-                    st.error("Please enter a valid OpenAI API Key.")
-                elif type(e) == openai.error.ServiceUnavailableError:
-                    st.error("OpenAI Service is currently unavailable. Please try again a short time later.")                   
-                else:
-                    st.error("Unfortunately the code generated from the model contained errors and was unable to execute. ")
+    api_keys_entered = True
+    # Check API keys are entered.
+    if  "ChatGPT-4" in model_list or "ChatGPT-3.5" in model_list or "GPT-3" in model_list or "GPT-3.5 Instruct" in model_list:
+        if not openai_key.startswith('sk-'):
+            st.error("Please enter a valid OpenAI API key.")
+            api_keys_entered = False
+    if "Code Llama" in model_list:
+        if not hf_key.startswith('hf_'):
+            st.error("Please enter a valid HuggingFace API key.")
+            api_keys_entered = False
+    if api_keys_entered:
+        # Place for plots depending on how many models
+        plots = st.columns(model_count)
+        # Get the primer for this dataset
+        primer1,primer2 = get_primer(datasets[chosen_dataset],'datasets["'+ chosen_dataset + '"]') 
+        # Create model, run the request and print the results
+        for plot_num, model_type in enumerate(model_list):
+            with plots[plot_num]:
+                st.subheader(model_type)
+                try:
+                    # Format the question 
+                    question_to_ask = format_question(primer1, primer2, question, model_type)   
+                    # Run the question
+                    answer=""
+                    answer = run_request(question_to_ask, available_models[model_type], key=openai_key,alt_key=hf_key)
+                    # the answer is the completed Python script so add to the beginning of the script to it.
+                    answer = primer2 + answer
+                    print("Model: " + model_type)
+                    print(answer)
+                    plot_area = st.empty()
+                    plot_area.pyplot(exec(answer))           
+                except Exception as e:
+                    if type(e) == openai.error.APIError:
+                        st.error("OpenAI API Error. Please try again a short time later. (" + str(e) + ")")
+                    elif type(e) == openai.error.Timeout:
+                        st.error("OpenAI API Error. Your request timed out. Please try again a short time later. (" + str(e) + ")")
+                    elif type(e) == openai.error.RateLimitError:
+                        st.error("OpenAI API Error. You have exceeded your assigned rate limit. (" + str(e) + ")")
+                    elif type(e) == openai.error.APIConnectionError:
+                        st.error("OpenAI API Error. Error connecting to services. Please check your network/proxy/firewall settings. (" + str(e) + ")")
+                    elif type(e) == openai.error.InvalidRequestError:
+                        st.error("OpenAI API Error. Your request was malformed or missing required parameters. (" + str(e) + ")")
+                    elif type(e) == openai.error.AuthenticationError:
+                        st.error("Please enter a valid OpenAI API Key. (" + str(e) + ")")
+                    elif type(e) == openai.error.ServiceUnavailableError:
+                        st.error("OpenAI Service is currently unavailable. Please try again a short time later. (" + str(e) + ")")               
+                    else:
+                        st.error("Unfortunately the code generated from the model contained errors and was unable to execute.")
 
 # Display the datasets in a list of tabs
 # Create the tabs
@@ -136,7 +158,7 @@ for dataset_num, tab in enumerate(tab_list):
         dataset_name = list(datasets.keys())[dataset_num]
         st.subheader(dataset_name)
         st.dataframe(datasets[dataset_name],hide_index=True)
-        
+
 # Insert footer to reference dataset origin  
 footer="""<style>.footer {position: fixed;left: 0;bottom: 0;width: 100%;text-align: center;}</style><div class="footer">
 <p> <a style='display: block; text-align: center;'> Datasets courtesy of NL4DV, nvBench and ADVISor </a></p></div>"""
--- classes.py
+++ classes.py
@@ -5,41 +5,34 @@
 #################################################################################
 
 import openai
+from langchain import HuggingFaceHub, LLMChain,PromptTemplate
 
-def run_request(question_to_ask, model_type, key):
-    if model_type == "gpt-4":
-        task = "Generate Python Code Script. The script should only include code, no comments."
-    elif model_type == "gpt-3.5-turbo":
+def run_request(question_to_ask, model_type, key, alt_key):
+    if model_type == "gpt-4" or model_type == "gpt-3.5-turbo" :
+        # Run OpenAI ChatCompletion API
         task = "Generate Python Code Script."
-    if model_type == "gpt-4" or model_type == "gpt-3.5-turbo":
-        # Run ChatGPT API
+        if model_type == "gpt-4":
+            # Ensure GPT-4 does not include additional comments
+            task = task + " The script should only include code, no comments."
         openai.api_key = key
-        response = openai.ChatCompletion.create(
-            model=model_type,
-            messages=[
-                {"role":"system","content":task},
-                {"role":"user","content":question_to_ask}])
-        res = response["choices"][0]["message"]["content"]
+        response = openai.ChatCompletion.create(model=model_type,
+            messages=[{"role":"system","content":task},{"role":"user","content":question_to_ask}])
+        llm_response = response["choices"][0]["message"]["content"]
+    elif model_type == "text-davinci-003" or model_type == "gpt-3.5-turbo-instruct":
+        # Run OpenAI Completion API
+        openai.api_key = key
+        response = openai.Completion.create(engine=model_type,prompt=question_to_ask,temperature=0,max_tokens=500,
+                    top_p=1.0,frequency_penalty=0.0,presence_penalty=0.0,stop=["plt.show()"])
+        llm_response = response["choices"][0]["text"] 
     else:
-        # We use the API to submit the query
-        openai.api_key = key
-        response = openai.Completion.create(
-            # model_type = text-davinci-003, code-davinci-002
-            engine=model_type,
-            prompt=question_to_ask,
-            temperature=0,
-            max_tokens=500,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-            stop=["plt.show()"]
-            )
-        res = response["choices"][0]["text"] 
+        # Hugging Face model
+        llm = HuggingFaceHub(huggingfacehub_api_token = alt_key, repo_id="codellama/" + model_type, model_kwargs={"temperature":0.1, "max_new_tokens":500})
+        llm_prompt = PromptTemplate.from_template(question_to_ask)
+        llm_chain = LLMChain(llm=llm,prompt=llm_prompt)
+        llm_response = llm_chain.predict()
     # rejig the response
-    res = format_response(res)
-    return res
-
-
+    llm_response = format_response(llm_response)
+    return llm_response
 
 def format_response( res):
     # Remove the load_csv from the answer if it exists
@@ -61,7 +54,13 @@ def format_response( res):
         res = res_before + res_after
     return res
 
-def format_question(primer_desc,primer_code , question):
+def format_question(primer_desc,primer_code , question, model_type):
+    # Fill in the model_specific_instructions variable
+    instructions = ""
+    if model_type == "Code Llama":
+        # Code llama tends to misuse the "c" argument when creating scatter plots
+        instructions = "\nDo not use the 'c' argument in the plot function, use 'color' instead and only pass color names like 'green', 'red', 'blue'."
+    primer_desc = primer_desc.format(instructions)  
     # Put the question at the end of the description primer within quotes, then add on the code primer.
     return  '"""\n' + primer_desc + question + '\n"""\n' + primer_code
 
@@ -80,11 +79,10 @@ def get_primer(df_dataset,df_name):
             primer_desc = primer_desc + "\nThe column '" + i + "' is type " + str(df_dataset.dtypes[i]) + " and contains numeric values. "   
     primer_desc = primer_desc + "\nLabel the x and y axes appropriately."
     primer_desc = primer_desc + "\nAdd a title. Set the fig suptitle as empty."
+    primer_desc = primer_desc + "{}" # Space for additional instructions if needed
     primer_desc = primer_desc + "\nUsing Python version 3.9.12, create a script using the dataframe df to graph the following: "
     pimer_code = "import pandas as pd\nimport matplotlib.pyplot as plt\n"
     pimer_code = pimer_code + "fig,ax = plt.subplots(1,1,figsize=(10,4))\n"
     pimer_code = pimer_code + "ax.spines['top'].set_visible(False)\nax.spines['right'].set_visible(False) \n"
     pimer_code = pimer_code + "df=" + df_name + ".copy()\n"
     return primer_desc,pimer_code
-
-
